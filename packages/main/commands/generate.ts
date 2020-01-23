82ea7e810a328c03d543aa55708fc9d78bcb4b12
khai96_ <hvksmr1996@gmail.com>
2020-01-23 21:17:24 +0700

    Rename generate to cmdGenerate

--- packages/main/commands/generate.ts
+++ packages/main/commands/generate.ts
@@ -6,7 +6,7 @@ import { SchemaWriter, listConfigFiles } from '@ts-schema-autogen/lib'
 import { ModuleSet } from '../module-set'
 import parsers from '../parsers'
 
-export async function generate<Prog, Def> (param: generate.Param<Prog, Def>) {
+export async function cmdGenerate<Prog, Def> (param: cmdGenerate.Param<Prog, Def>) {
   const { modules } = param
   const { console } = modules
   const { basename, ignored } = param.args
@@ -28,7 +28,7 @@ export async function generate<Prog, Def> (param: generate.Param<Prog, Def>) {
   return Status.Success
 }
 
-export namespace generate {
+export namespace cmdGenerate {
   export interface Param<Program, Definition> {
     readonly modules: ModuleSet<Program, Definition>
     readonly args: CliArguments
