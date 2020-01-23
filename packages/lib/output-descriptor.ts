539bb97ed6aa20d59da6d1482f45638c6a379d0f
khai96_ <hvksmr1996@gmail.com>
2020-01-23 14:06:36 +0700

    Forgot jsdoc for output-descriptor.ts

--- packages/lib/output-descriptor.ts
+++ packages/lib/output-descriptor.ts
@@ -2,10 +2,18 @@ import { MaybeArray, ensureArray } from '@ts-schema-autogen/utils'
 import { OutputDescriptor } from '@ts-schema-autogen/types'
 export { OutputDescriptor }
 
+/**
+ * Convert an output filename or descriptor into a descriptor
+ * @param maybeDesc Output filename or output descriptor
+ */
 export const ensureOutputDescriptor =
   (maybeDesc: string | OutputDescriptor) =>
     typeof maybeDesc === 'string' ? { filename: maybeDesc } : maybeDesc
 
+/**
+ * Convert an output filename, descriptor, or array of either into an array of descriptors
+ * @param maybeDesc Output filename, output descriptor or array of either
+ */
 export const ensureOutputDescriptorArray =
   (maybeDesc: MaybeArray<string | OutputDescriptor>): OutputDescriptor[] =>
     ensureArray(maybeDesc).map(ensureOutputDescriptor)
