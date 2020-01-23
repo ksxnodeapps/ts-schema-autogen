7ab555efd0c92482cffda876ebca1c0521ff53f5
khai96_ <hvksmr1996@gmail.com>
2020-01-23 21:49:06 +0700

    Complete cmdClean

---
+++ packages/main/commands/clean.ts
@@ -0,0 +1,27 @@
+import { objectExtends } from '@tsfun/object'
+import { clean, listConfigFiles } from '@ts-schema-autogen/lib'
+import { Status } from '@ts-schema-autogen/status'
+import { base } from '../param'
+import parsers from '../parsers'
+
+export interface CleanParam extends base.Param<unknown, unknown> {}
+
+export async function cmdClean (param: CleanParam) {
+  const { modules, args } = param
+  const { console } = modules
+  const cleanResult = await clean(objectExtends(modules, {
+    parsers: await parsers,
+    configFiles: listConfigFiles(objectExtends(modules, {
+      root: modules.process.cwd(),
+      basename: args.basename,
+      ignored: args.ignored
+    }))
+  }))
+  if (cleanResult.code) {
+    console.error('[ERROR] Failed to clean')
+    console.error(cleanResult) // TODO: Implement toString and use it
+    return cleanResult.code
+  }
+  console.info('[SUCCESS]')
+  return Status.Success
+}
