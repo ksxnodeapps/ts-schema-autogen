a0af9d7b22c6bc8cd7739c1882c4fc4cb2a6b94e
khai96_ <hvksmr1996@gmail.com>
2020-01-23 21:08:58 +0700

    Complete generate.ts

--- packages/main/generate.ts
+++ packages/main/generate.ts
@@ -1,16 +1,31 @@
+import { pass } from '@tsfun/pipe'
 import { addProperty, objectExtends } from '@tsfun/object'
+import { getAsyncArray } from '@ts-schema-autogen/utils'
 import { Status } from '@ts-schema-autogen/status'
 import { SchemaWriter, listConfigFiles } from '@ts-schema-autogen/lib'
 import { ModuleSet } from './module-set'
 import parsers from './parsers'
 
-export async function generate<Prog, Def> (param: generate.Param<Prog, Def>): Promise<Status> {
+export async function generate<Prog, Def> (param: generate.Param<Prog, Def>) {
   const { modules } = param
+  const { console } = modules
   const { basename, ignored } = param.args
   const root = modules.process.cwd()
-  const configFilesIterator = listConfigFiles(objectExtends(modules, { root, basename, ignored }))
+  const configFilesPromise = pass(modules)
+    .to(objectExtends, { root, basename, ignored })
+    .to(listConfigFiles)
+    .to(getAsyncArray)
+    .get()
   const writer = new SchemaWriter(addProperty(modules, 'parsers', await parsers))
-  writer.writeSchemas(configFilesIterator)
+  const writeResult = await writer.writeSchemas(await configFilesPromise)
+  if (writeResult.code) {
+    console.error('[ERROR] Failed to generate JSON schemas')
+    console.error(writeResult) // TODO: Implement Failure::toString() and use it here
+    return writeResult.code
+  }
+
+  console.info('[SUCCESS]')
+  return Status.Success
 }
 
 export namespace generate {
