06f6aea93c11e1f61d6e66fb723c858bd27627b1
khai96_ <hvksmr1996@gmail.com>
2020-01-26 10:12:18 +0700

    Create test-utils/fs

--- tools/test-utils/fs.ts
+++ tools/test-utils/fs.ts
@@ -1,4 +1,14 @@
 import { StringPathFileSystem } from 'simple-fake-fs'
 import { FSX } from '@ts-schema-autogen/types'
 
-export class FakeFileSystem extends StringPathFileSystem implements FSX.Mod {}
+export class FakeFileSystem extends StringPathFileSystem implements FSX.Mod {
+  private mkfn<Xs extends any[], Y> (fn: (...xs: Xs) => Y) {
+    return jest.fn(async (...xs: Xs) => fn.apply(this, xs))
+  }
+
+  public readonly stat = this.mkfn(this.statSync)
+  public readonly readdir = this.mkfn(this.readdirSync)
+  public readonly readFile = this.mkfn(this.readFileSync)
+  public readonly writeFile = this.mkfn(this.writeFileSync)
+  public readonly remove = jest.fn()
+}
