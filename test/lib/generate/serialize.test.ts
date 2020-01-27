eafd7cfd19cc19eb5bcd5c839b2b06dc416ecec4
khai96_ <hvksmr1996@gmail.com>
2020-01-27 08:50:07 +0700

    Create test for generate.ts#serialize

---
+++ test/lib/generate/serialize.test.ts
@@ -0,0 +1,28 @@
+import { serialize } from '@ts-schema-autogen/lib'
+
+const object = {
+  a: {
+    b: 'ab'
+  },
+  c: [0, 1, 2]
+}
+
+it('indent = 2', () => {
+  expect(serialize(object, { indent: 2, filename: undefined! }))
+    .toBe(JSON.stringify(object, undefined, 2) + '\n')
+})
+
+it('indent = "tab"', () => {
+  expect(serialize(object, { indent: 'tab', filename: undefined! }))
+    .toBe(JSON.stringify(object, undefined, '\t') + '\n')
+})
+
+it('indent = "none"', () => {
+  expect(serialize(object, { indent: 'none', filename: undefined! }))
+    .toBe(JSON.stringify(object) + '\n')
+})
+
+it('indent = default', () => {
+  expect(serialize(object, { filename: undefined! }))
+    .toBe(JSON.stringify(object, undefined, 2) + '\n')
+})
