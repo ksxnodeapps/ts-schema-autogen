b6a4842511a045149c4d0c1a5fdbb6638080f1ae
khai96_ <hvksmr1996@gmail.com>
2020-01-29 21:48:23 +0700

    Extra subTree

--- test/fixtures/fs-tree.ts
+++ test/fixtures/fs-tree.ts
@@ -9,139 +9,142 @@ import {
   OutputDescriptor
 } from '@ts-schema-autogen/types'
 
-const dump = <X> (x: X) => safeDump(x)
-const dumpConfig = (config: Config) => dump(config)
+interface Dump {
+  (config: Config): string
+}
 
-export const fsTree: FsTree = {
-  yaml: {
-    'single-symbol': {
-      'single-output': {
-        'output-filename': {
-          '.schema.autogen.yaml': dumpConfig({
-            generator: undefined!,
-            instruction: unit<SingleSymbolInstruction>({
-              symbol: 'Foo',
-              output: 'output.schema.json'
-            })
-          })
-        },
-        'output-descriptor': dumpConfig({
+const subTree = (dump: Dump) => ({
+  'single-symbol': {
+    'single-output': {
+      'output-filename': {
+        '.schema.autogen.yaml': dump({
           generator: undefined!,
           instruction: unit<SingleSymbolInstruction>({
             symbol: 'Foo',
-            output: unit<OutputDescriptor>({
-              filename: 'output.schema.json',
-              format: 'json',
-              indent: 'tab'
-            })
+            output: 'output.schema.json'
           })
         })
       },
-      'multiple-output': {
-        'output-filename': {
-          '.schema.autogen.yaml': dumpConfig({
-            generator: undefined!,
-            instruction: unit<SingleSymbolInstruction>({
-              symbol: 'Foo',
-              output: 'output.schema.json'
-            })
+      'output-descriptor': dump({
+        generator: undefined!,
+        instruction: unit<SingleSymbolInstruction>({
+          symbol: 'Foo',
+          output: unit<OutputDescriptor>({
+            filename: 'output.schema.json',
+            format: 'json',
+            indent: 'tab'
           })
-        },
-        'output-descriptor': {
-          '.schema.autogen.yaml': dumpConfig({
-            generator: undefined!,
-            instruction: unit<SingleSymbolInstruction>({
-              symbol: 'Foo',
-              output: [
-                'non-descriptor.schema.json',
-                unit<OutputDescriptor>({
-                  filename: 'default-descriptor.schema.json'
-                }),
-                unit<OutputDescriptor>({
-                  filename: 'custom-descriptor.schema.json',
-                  format: 'json',
-                  indent: 'tab'
-                })
-              ]
-            })
+        })
+      })
+    },
+    'multiple-output': {
+      'output-filename': {
+        '.schema.autogen.yaml': dump({
+          generator: undefined!,
+          instruction: unit<SingleSymbolInstruction>({
+            symbol: 'Foo',
+            output: 'output.schema.json'
           })
-        }
+        })
+      },
+      'output-descriptor': {
+        '.schema.autogen.yaml': dump({
+          generator: undefined!,
+          instruction: unit<SingleSymbolInstruction>({
+            symbol: 'Foo',
+            output: [
+              'non-descriptor.schema.json',
+              unit<OutputDescriptor>({
+                filename: 'default-descriptor.schema.json'
+              }),
+              unit<OutputDescriptor>({
+                filename: 'custom-descriptor.schema.json',
+                format: 'json',
+                indent: 'tab'
+              })
+            ]
+          })
+        })
+      }
+    }
+  },
+  'multi-symbol': {
+    'single-output': {
+      'output-filename': {
+        '.schema.autogen.yaml': dump({
+          generator: undefined!,
+          instruction: unit<MultiSymbolInstruction>({
+            list: [
+              {
+                output: 'foo.schema.json',
+                symbol: 'Foo'
+              },
+              {
+                output: 'bar.schema.json',
+                symbol: 'Bar'
+              }
+            ]
+          })
+        })
+      },
+      'output-descriptor': {
+        '.schema.autogen.yaml': dump({
+          generator: undefined!,
+          instruction: unit<MultiSymbolInstruction>({
+            list: [
+              {
+                output: 'foo.schema.json',
+                symbol: 'Foo'
+              },
+              {
+                output: 'bar.schema.json',
+                symbol: 'Bar'
+              }
+            ]
+          })
+        })
       }
     },
-    'multi-symbol': {
-      'single-output': {
-        'output-filename': {
-          '.schema.autogen.yaml': dumpConfig({
-            generator: undefined!,
-            instruction: unit<MultiSymbolInstruction>({
-              list: [
-                {
-                  output: 'foo.schema.json',
-                  symbol: 'Foo'
-                },
-                {
-                  output: 'bar.schema.json',
-                  symbol: 'Bar'
-                }
-              ]
-            })
+    'multi-output': {
+      'output-filename': {
+        '.schema.autogen.yaml': dump({
+          generator: undefined!,
+          instruction: unit<MultiSymbolInstruction>({
+            list: [
+              {
+                output: [
+                  'foo.filename.schema.json',
+                  {
+                    filename: 'foo.default.schema.json'
+                  },
+                  {
+                    filename: 'foo.custom.schema.json',
+                    format: 'json',
+                    indent: 'tab'
+                  }
+                ],
+                symbol: 'Foo'
+              },
+              {
+                output: [
+                  {
+                    filename: 'bar.4.schema.json',
+                    format: 'json',
+                    indent: 4
+                  }
+                ],
+                symbol: 'Bar'
+              }
+            ]
           })
-        },
-        'output-descriptor': {
-          '.schema.autogen.yaml': dumpConfig({
-            generator: undefined!,
-            instruction: unit<MultiSymbolInstruction>({
-              list: [
-                {
-                  output: 'foo.schema.json',
-                  symbol: 'Foo'
-                },
-                {
-                  output: 'bar.schema.json',
-                  symbol: 'Bar'
-                }
-              ]
-            })
-          })
-        }
-      },
-      'multi-output': {
-        'output-filename': {
-          '.schema.autogen.yaml': dumpConfig({
-            generator: undefined!,
-            instruction: unit<MultiSymbolInstruction>({
-              list: [
-                {
-                  output: [
-                    'foo.filename.schema.json',
-                    {
-                      filename: 'foo.default.schema.json'
-                    },
-                    {
-                      filename: 'foo.custom.schema.json',
-                      format: 'json',
-                      indent: 'tab'
-                    }
-                  ],
-                  symbol: 'Foo'
-                },
-                {
-                  output: [
-                    {
-                      filename: 'bar.4.schema.json',
-                      format: 'json',
-                      indent: 4
-                    }
-                  ],
-                  symbol: 'Bar'
-                }
-              ]
-            })
-          })
-        }
+        })
       }
     }
   }
+})
+
+export const fsTree: FsTree = {
+  yaml: subTree(safeDump)
 }
 
 export default fsTree
