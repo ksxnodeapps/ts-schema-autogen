d0171916b5e1a81e2229fa54e4edc91fb7ee41f7
khai96_ <hvksmr1996@gmail.com>
2020-01-31 20:57:38 +0700

    Use simple-fake-path

--- tools/test-utils/path.ts
+++ tools/test-utils/path.ts
@@ -1,29 +1,8 @@
+import Base, { symCwd, symRoot } from 'simple-fake-path'
 import { Path } from '@ts-schema-autogen/types'
 
-export class FakePath implements Path.Mod {
-  public readonly isAbsolute = (path: string): boolean => path.startsWith('/')
-
-  public readonly join = (...paths: string[]): string => {
-    if (!paths.length) return '.'
-    const [head, ...rest] = paths
-    const tail = this.join(...rest)
-    if (head === '.') return tail
-    if (tail === '.') return head
-    if (tail.startsWith('../')) return this.join(this.dirname(head), tail.slice('../'.length))
-    const left = head.endsWith('/') ? head.slice(0, -1) : head
-    const right = tail.startsWith('/') ? tail.slice(1) : tail
-    return left + '/' + right
-  }
-
-  public readonly dirname = (path: string): string => {
-    if (path === '' || path === '.' || path === '/') return path
-    const segments = path.split('/').slice(0, -1)
-    if (!segments.length) return '.'
-    return this.join(...segments)
-  }
-
-  public readonly basename = (path: string): string => {
-    const segments = path.split('/')
-    return segments[segments.length - 1]
-  }
+export class FakePath extends Base implements Path.Mod {
+  public readonly [symCwd] = undefined!
+  public readonly [symRoot] = ['/']
+  public readonly sep = '/'
 }
