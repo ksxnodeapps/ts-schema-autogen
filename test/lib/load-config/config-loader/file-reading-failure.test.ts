e965c47551720472a3902b19891af65e896acfbb
khai96_ <hvksmr1996@gmail.com>
2020-02-02 14:43:41 +0700

    Create test for FileReadingFailure branch

---
+++ test/lib/load-config/config-loader/file-reading-failure.test.ts
@@ -0,0 +1,63 @@
+import validCfgOnly from '../../../fixtures/fs-tree/valid-config-only'
+import { FakeFileSystem, FakePath } from '@tools/test-utils'
+
+import {
+  Status,
+  FileReadingFailure,
+  ConfigLoader,
+  createYamlFormatDescriptor,
+  createJsonFormatDescriptor
+} from '@ts-schema-autogen/lib'
+
+const getParsers = () => Promise.all([
+  createJsonFormatDescriptor('JSON Parser'),
+  createYamlFormatDescriptor('YAML Parser')
+])
+
+async function setup (filename: string) {
+  const fsx = new FakeFileSystem('/', validCfgOnly)
+  const path = new FakePath()
+  const configLoader = new ConfigLoader({
+    fsx,
+    path,
+    parsers: await getParsers()
+  })
+  const result = await configLoader.loadConfig(filename)
+  return { filename, fsx, path, configLoader, result }
+}
+
+describe('when a directory is read as file', () => {
+  const cfgFile = 'json/single-symbol/single-output/output-filename'
+
+  it('result matches snapshot', async () => {
+    const { result } = await setup(cfgFile)
+    expect(result).toMatchSnapshot({
+      code: Status.FileReadingFailure,
+      error: expect.anything(),
+      path: cfgFile
+    })
+  })
+
+  it('result is a FileReadingFailure', async () => {
+    const { result } = await setup(cfgFile)
+    expect(result).toBeInstanceOf(FileReadingFailure)
+  })
+})
+
+describe('when file does not exist', () => {
+  const cfgFile = '[DOES NOT EXIST]'
+
+  it('result matches snapshot', async () => {
+    const { result } = await setup(cfgFile)
+    expect(result).toMatchSnapshot({
+      code: Status.FileReadingFailure,
+      error: expect.anything(),
+      path: cfgFile
+    })
+  })
+
+  it('result is a FileReadingFailure', async () => {
+    const { result } = await setup(cfgFile)
+    expect(result).toBeInstanceOf(FileReadingFailure)
+  })
+})
