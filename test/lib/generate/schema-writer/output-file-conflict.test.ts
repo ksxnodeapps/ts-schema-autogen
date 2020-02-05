33005681f9b998b80026fa97a973a0a3d6c70c58
khai96_ <hvksmr1996@gmail.com>
2020-02-05 13:13:34 +0700

    Add more tests

--- test/lib/generate/schema-writer/output-file-conflict.test.ts
+++ test/lib/generate/schema-writer/output-file-conflict.test.ts
@@ -45,3 +45,72 @@ describe('obvious conflicts', () => {
     expect(fsx.outputFile).not.toBeCalled()
   })
 })
+
+describe('not obvious conflicts', () => {
+  const configPaths = [
+    'not-obvious-conflicts/foo/.schema.autogen.json',
+    'not-obvious-conflicts/bar.schema.autogen.json'
+  ]
+
+  it('returns result matching snapshot', async () => {
+    const { result } = await setup(configPaths)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('returns an OutputFileConflict', async () => {
+    const { result } = await setup(configPaths)
+    expect(result).toBeInstanceOf(OutputFileConflict)
+  })
+
+  it('does not call outputFile', async () => {
+    const { fsx } = await setup(configPaths)
+    expect(fsx.outputFile).not.toBeCalled()
+  })
+})
+
+describe('more than one conflicts', () => {
+  const configPaths = [
+    'more-than-one-conflicts/foo.json',
+    'more-than-one-conflicts/bar.json',
+    'more-than-one-conflicts/baz.json'
+  ]
+
+  it('returns result matching snapshot', async () => {
+    const { result } = await setup(configPaths)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('returns an OutputFileConflict', async () => {
+    const { result } = await setup(configPaths)
+    expect(result).toBeInstanceOf(OutputFileConflict)
+  })
+
+  it('does not call outputFile', async () => {
+    const { fsx } = await setup(configPaths)
+    expect(fsx.outputFile).not.toBeCalled()
+  })
+})
+
+describe('mixed with non conflicts', () => {
+  const configPaths = [
+    'mixed-with-non-conflicts/conflicted-foo.json',
+    'mixed-with-non-conflicts/conflicted-bar.json',
+    'mixed-with-non-conflicts/okay-foo.json',
+    'mixed-with-non-conflicts/okay-bar.json'
+  ]
+
+  it('returns result matching snapshot', async () => {
+    const { result } = await setup(configPaths)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('returns an OutputFileConflict', async () => {
+    const { result } = await setup(configPaths)
+    expect(result).toBeInstanceOf(OutputFileConflict)
+  })
+
+  it('does not call outputFile', async () => {
+    const { fsx } = await setup(configPaths)
+    expect(fsx.outputFile).not.toBeCalled()
+  })
+})
