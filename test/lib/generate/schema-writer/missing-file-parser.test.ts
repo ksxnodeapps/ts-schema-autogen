58a1fa601d69813421902a6be8f0ed4d2f086873
khai96_ <hvksmr1996@gmail.com>
2020-02-05 16:47:16 +0700

    Create test for MissingFileParser

---
+++ test/lib/generate/schema-writer/missing-file-parser.test.ts
@@ -0,0 +1,54 @@
+import fsTree from '../../../fixtures/fs-tree/valid-config-only'
+import TJS from '../../../fixtures/tjs'
+import { FakeFileSystem, FakePath, printResult } from '@tools/test-utils'
+
+import {
+  Status,
+  MissingFileParser,
+  SchemaWriter
+} from '@ts-schema-autogen/lib'
+
+async function setup (configPaths: readonly string[]) {
+  const fsx = new FakeFileSystem('/', fsTree)
+  const path = new FakePath()
+  const tjs = new TJS()
+  const schemaWriter = new SchemaWriter({
+    fsx,
+    path,
+    tjs,
+    parsers: []
+  })
+  const result = await schemaWriter.writeSchemas(configPaths)
+  return { fsx, path, tjs, result }
+}
+
+const configPaths = [
+  'yaml/single-symbol/single-output/output-filename/.schema.autogen.yaml',
+  'yaml/single-symbol/single-output/output-descriptor/.schema.autogen.yaml',
+  'yaml/single-symbol/multiple-output/output-filename/.schema.autogen.yaml'
+]
+
+it('returns result matching snapshot', async () => {
+  const { result } = await setup(configPaths)
+  expect(result).toMatchSnapshot()
+})
+
+it('returns a MissingFileParser', async () => {
+  const { result } = await setup(configPaths)
+  expect(result).toBeInstanceOf(MissingFileParser)
+})
+
+it('returns result containing expected properties', async () => {
+  const { result } = await setup(configPaths)
+  expect(result).toHaveProperty('code', Status.MissingFileParser)
+})
+
+it('does not call outputFile', async () => {
+  const { fsx } = await setup(configPaths)
+  expect(fsx.outputFile).not.toBeCalled()
+})
+
+it('error messages', async () => {
+  const { result } = await setup(configPaths)
+  expect(printResult(result)).toMatchSnapshot()
+})
