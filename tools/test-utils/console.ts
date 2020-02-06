0716dc074d8f34cc0d8121bca78ffeee9b25c49e
khai96_ <hvksmr1996@gmail.com>
2020-02-06 18:43:34 +0700

    Add methods that convert FakeConsole to string

--- tools/test-utils/console.ts
+++ tools/test-utils/console.ts
@@ -1,8 +1,19 @@
-import { ConsoleInstance } from 'simple-fake-console'
+import { ConsoleInstance, ActionType, getString } from 'simple-fake-console'
 import { Console } from '@ts-schema-autogen/types'
 
 export class FakeConsole implements Console.Mod {
   public readonly core = new ConsoleInstance()
   public readonly info = jest.fn(this.core.info)
   public readonly error = jest.fn(this.core.error)
+
+  public toString () {
+    return getString({
+      console: this.core,
+      types: [ActionType.Info, ActionType.Error]
+    })
+  }
+
+  public getString () {
+    return '\n' + this.toString() + '\n'
+  }
 }
