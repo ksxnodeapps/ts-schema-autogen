0752640086df1ed77b4250e6852773471940ecda
khai96_ <hvksmr1996@gmail.com>
2020-02-07 13:18:28 +0700

    Caching is not necessary
    as every file is only loaded once

--- packages/validate/load.ts
+++ packages/validate/load.ts
@@ -5,16 +5,10 @@ import { readFileSync } from 'fs'
 export { Schema }
 
 export class SchemaLoader {
-  private readonly cache = new Map<string, Schema>()
-
   public load (name: string): Schema {
-    const { cache } = this
-    if (cache.has(name)) return cache.get(name)!
     const filename = path.join(__dirname, 'schemas', name + '.schema.json')
     const text = readFileSync(filename, 'utf8')
-    const definition: Schema = JSON.parse(text)
-    cache.set(name, definition)
-    return definition
+    return JSON.parse(text)
   }
 }
 
