6e0208a6fabff64ed578a9c19df02badb72a210f
khai96_ <hvksmr1996@gmail.com>
2020-02-07 12:36:16 +0700

    Use fs-extra and path directly

--- packages/validate/load.ts
+++ packages/validate/load.ts
@@ -1,28 +1,16 @@
+import path from 'path'
 import { Schema } from 'jsonschema'
-import { Path, FSX } from '@ts-schema-autogen/types'
+import { readFile } from 'fs-extra'
 
 export { Schema }
-export type JoinPath = Path.Mod['join']
-export type ReadFile = FSX.Mod['readFile']
 
 export class SchemaLoader {
   private readonly cache = new Map<string, Schema>()
 
-  constructor (
-    private readonly joinPath: JoinPath,
-    private readonly readFile: ReadFile
-  ) {}
-
   public async load (name: string): Promise<Schema> {
-    const {
-      cache,
-      joinPath,
-      readFile
-    } = this
-
+    const { cache } = this
     if (cache.has(name)) return cache.get(name)!
-
-    const filename = joinPath(__dirname, 'schemas', name + '.schema.json')
+    const filename = path.join(__dirname, 'schemas', name + '.schema.json')
     const text = await readFile(filename, 'utf8')
     const definition: Schema = JSON.parse(text)
     cache.set(name, definition)
