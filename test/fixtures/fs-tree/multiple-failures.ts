bc2588df5a339883ab0c4e9ec2cc5713b9dbaa00
khai96_ <hvksmr1996@gmail.com>
2020-02-09 17:14:53 +0700

    Rename fixture

--- test/fixtures/fs-tree/multiple-failures.ts
+++ test/fixtures/fs-tree/multiple-failures.ts
@@ -5,7 +5,7 @@ const def = <Value> (mod: { readonly default: Value }) => mod.default
 export const createFsTree = async (): Promise<FsTree> => ({
   'circular-reference': await import('./circular-reference').then(def),
   'file-reading-failure': await import('./file-reading-failure').then(def),
-  'file-tree-removal-failure': await import('./file-tree-removal-failure').then(def),
+  'file-removal-failure': await import('./file-removal-failure').then(def),
   'output-file-conflict': await import('./output-file-conflict').then(def),
   'text-parsing-failure': await import('./text-parsing-failure').then(def),
   'valid-config-only': await import('./valid-config-only').then(def)
